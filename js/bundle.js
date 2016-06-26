b900fd8821cc9d002ecd73146751d01e2d8a798d
jmaicaaan <jmsantosss29@yahoo.com>
2016-06-26 22:32:58 +0800

    Lalala

--- js/bundle.js
+++ js/bundle.js
@@ -7,11 +7,48 @@ function mainComponent(){
 		scope: {},
 		restrict: "E",
 		templateUrl: "../../views/main.html",
-		link: linker
+		link: linker,
+		controller: control,
+		controllerAs: "vm"
 	};
 
 	function linker(scope, elem, attrs){
 		
+	}
+
+	function control($mdSidenav, $mdPanel){
+		var self = this;
+
+		self.openNav = openNav;
+
+		openNav();
+
+		function openNav(event){
+			//$mdSidenav("nav").toggle();
+
+			// $mdDialog.show({
+			// 	parent: angular.element(document.body),
+			// 	targetEvent: event,
+			// 	templateUrl: "../../views/dialog.html"
+			// })
+
+			var config = {
+				attachTo: angular.element(document.body),
+				disableParentScroll: true,
+				templateUrl: "../../views/dialog.html",
+				hasBackdrop: true,
+				panelClass: "demo-dialog-example",
+				targetEvent: event,
+				position: $mdPanel.newPanelPosition().absolute().center(),
+				trapFocus: true,
+				zIndex: 150,
+				clickOutsideToClose: true,
+			    escapeToClose: true,
+			    focusOnOpen: true
+			};
+
+			$mdPanel.open(config);
+		}
 	}
 }
 },{}],2:[function(require,module,exports){
